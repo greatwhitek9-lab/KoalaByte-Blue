d40e0fc79bea654b7b90883aaa8ea41163c9ebd9
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-15 11:26:18 -0400

    Add Koala Kan Kommander runner

---
+++ scripts/run_koala_kan_kommander.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run the Koala Kan Kommander safe SocketCAN plug-in."""
+
+from koalablue.koala_kan_kommander import run_cli
+
+if __name__ == "__main__":
+    raise SystemExit(run_cli())
