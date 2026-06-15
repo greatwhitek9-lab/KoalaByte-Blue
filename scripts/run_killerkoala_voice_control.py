e257d3ad32cbb16c10bfb6bb19848e2c89009774
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-15 07:33:35 -0400

    RevA19: add killerkoala spoken command runner

---
+++ scripts/run_killerkoala_voice_control.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run killerkoala spoken-command module routing."""
+
+from koalablue.killerkoala_voice_control import run_cli
+
+if __name__ == "__main__":
+    raise SystemExit(run_cli())
