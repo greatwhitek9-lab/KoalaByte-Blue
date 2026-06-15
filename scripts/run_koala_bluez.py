39fe3c1781982570ffd78a4c1ef02d0369b70322
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-15 00:30:13 -0400

    Add Koala BlueZ runner

---
+++ scripts/run_koala_bluez.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run KoalaByte Blue local Bluetooth/BlueZ helper commands."""
+
+from koalablue.bluez_tools import run_cli
+
+if __name__ == "__main__":
+    raise SystemExit(run_cli())
