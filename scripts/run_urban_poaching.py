09df8013ce7b0a590d1a0fc1933ba6e42d718986
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-14 22:28:53 -0400

    Add Urban Poaching CLI runner

---
+++ scripts/run_urban_poaching.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run the Urban Poaching authorized BLE fox-hunt game."""
+
+from koalablue.urban_poaching import run_cli
+
+if __name__ == "__main__":
+    raise SystemExit(run_cli())
