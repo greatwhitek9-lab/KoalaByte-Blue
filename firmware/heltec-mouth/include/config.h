cfe9c11305a842a7e2bdd2dc9f17470b2e3771bf
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-22 02:35:01 -0400

    Add Heltec OLED config

---
+++ firmware/heltec-mouth/include/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define KOALA_OLED_FW_VERSION "0.1.0-heltec-koala-display"
+#define KOALA_OLED_SERIAL_BAUD 115200
+#define KOALA_OLED_SDA 17
+#define KOALA_OLED_SCL 18
+#define KOALA_OLED_RST 21
+#define KOALA_FACE_DEFAULT_DURATION_MS 4500
+#define KOALA_FACE_NAME "killerkoala"
