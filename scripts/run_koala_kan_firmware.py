35c328e9bacdfcd2c38732a4de7c6db88d7d5622
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-22 10:29:28 -0400

    Add Koala Kan firmware track runner

---
+++ scripts/run_koala_kan_firmware.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run the optional Koala Kan Commander firmware planning track."""
+
+from koalablue.koala_kan_firmware import run_cli
+
+if __name__ == "__main__":
+    raise SystemExit(run_cli())
