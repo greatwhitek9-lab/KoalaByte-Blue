6fece44c5156b48d7c8bde05cb2914ecd6190139
UrbanPoacher <greatwhitek9@gmail.com>
2026-06-28 13:19:53 -0400

    Auto-install GreatWhite Reef menu extension

--- pi-companion/koalablue/__init__.py
+++ pi-companion/koalablue/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.13.0"
+
+try:
+    from .greatwhite_reef import install_menu_catalog
+
+    install_menu_catalog()
+except Exception:
+    # Menu extensions must never prevent core package imports.
+    pass
