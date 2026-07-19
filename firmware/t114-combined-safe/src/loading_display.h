194ab7289b1ba5120a0c55c4a86d727b51184168
UrbanPoacher <greatwhitek9@gmail.com>
2026-07-19 11:40:51 -0400

    Add T114 mouth and menu display API

--- firmware/t114-combined-safe/src/loading_display.h
+++ firmware/t114-combined-safe/src/loading_display.h
@@ -6,6 +6,9 @@
 bool loading_display_init(void);
 bool loading_display_ready(void);
 void render_loading_banner(const char *banner);
+void render_killerkoala_mouth(const char *state, const char *message,
+                              bool mouth_open);
+void render_menu_status(const char *message);
 void loading_display_end(void);
 
 #endif
