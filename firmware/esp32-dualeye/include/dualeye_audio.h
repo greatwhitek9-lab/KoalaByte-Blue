d245ee4796ba3580826711e75ef8cb8bb6e86d6d
UrbanPoacher <greatwhitek9@gmail.com>
2026-08-30 20:34:57 -0400

    Use vendor TDM RX audio transport API

--- firmware/esp32-dualeye/include/dualeye_audio.h
+++ firmware/esp32-dualeye/include/dualeye_audio.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <Arduino.h>
-#include <ESP_I2S.h>
 
 bool dualEyeAudioBegin();
 bool dualEyeAudioReady();
@@ -8,12 +7,13 @@ bool dualEyeMicrophoneReady();
 bool dualEyeSpeakerReady();
 bool dualEyeAudioBusy();
 const char *dualEyeAudioStatus();
-I2SClass &dualEyeAudioBus();
 size_t dualEyeAudioRead(uint8_t *buffer, size_t length);
 uint32_t dualEyeAudioReadAttempts();
 size_t dualEyeAudioLastReadBytes();
+size_t dualEyeAudioLastRawReadBytes();
 uint32_t dualEyeAudioLastReadDurationMs();
 const char *dualEyeAudioLastReadState();
+uint8_t dualEyeAudioInputSlots();
 float dualEyeAudioRms16Stereo(const uint8_t *buffer, size_t length);
 bool dualEyeAudioWriteMono16(const int16_t *samples, size_t sampleCount);
 bool dualEyeAudioWriteStereo16(const uint8_t *data, size_t length);
